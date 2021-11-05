cd5cc04865c1c6a8b1aad7a9e7d3455c27d02621
FranciscoSorto <sortofrancisco24@gmail.com>
2021-11-05 11:01:27 -0400

    Blue Ferrari car is better than the lambo i created is in stash

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
-        
+
+        car.name = "Ferrari";
+        car.color = "Blue";
+        car.doors = 3;
+        car.engineCapacity = "60000cc";
+
+        System.out.println(car.color);
     }
 }
